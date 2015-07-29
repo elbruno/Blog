125fb1f67576dd115b6c76ee1bbb8330bc25de2d
El Bruno <brunocapuano@msn.com>
2015-07-29 13:19:24 +0200

    sample for nameof

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new People();
+            var address = p.Address;
+            Console.WriteLine("Variables");
+            Console.WriteLine(nameof(p));         // p
+            Console.WriteLine(nameof(p.Address)); // Address
+            Console.WriteLine(nameof(p.Email));   // EMail
+            Console.WriteLine(nameof(address));   // address
+            Console.WriteLine("");
+            Console.WriteLine("Members");
+            Console.WriteLine(nameof(p.GetCompleteName)); // GetCompleteName
+            Console.WriteLine(nameof(string.GetType));    // GetType
+            Console.ReadLine();
+        }
+    }
+
+    class People
+    {
+        public string Email { get; set; }
+        public string Address { get; set; }
+
+        public string GetCompleteName()
+        {
+            return string.Empty;
+        }
+    }
+}

9e17c3af01698b163f363f8643c0e99fa7538220
El Bruno <bcapuano@gmail.com>
2015-08-29 21:02:34 +0200

    + upgrade version number
    + complete information for 3rd blog post and to pusblish in VSGallery

--- ElBruno.RemoveComments V02/ElBruno.RemoveComments/ElBruno.RemoveComments/Properties/AssemblyInfo.cs
+++ ElBruno.RemoveComments V02/ElBruno.RemoveComments/ElBruno.RemoveComments/Properties/AssemblyInfo.cs
@@ -1,31 +1,14 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ElBruno.RemoveComments")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("El Bruno - Remove Comments, Roslyn Sample")]
+[assembly: AssemblyDescription("El Bruno - Remove Comments, Roslyn Sample")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ElBruno.RemoveComments")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("Bruno Capuano")]
+[assembly: AssemblyProduct("El Bruno Blog")]
+[assembly: AssemblyCopyright("Copyright © Bruno Capuano 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.50829.1")]
+[assembly: AssemblyFileVersion("1.0.50829.1")]

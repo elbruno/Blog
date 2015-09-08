3bbae2f5163320a1978836c8f7ddc93353be56e6
El Bruno <bcapuano@gmail.com>
2015-09-08 10:46:51 +0200

    + Sample to list all found BLE devices in Android
    app

---
+++ Xamarin/AndroidApp4/Components/Monkey.Robotics-0.5.2/samples/BLE Explorer/Robotics.Mobile.BtLEExplorer.iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+using Robotics.Mobile.Core.Bluetooth.LE;
+using Xamarin.Forms.Platform.iOS;
+
+namespace Robotics.Mobile.BtLEExplorer.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : FormsApplicationDelegate // new int 1.3
+	{
+		UIWindow window;
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			Xamarin.Forms.Forms.Init ();
+
+			window = new UIWindow (UIScreen.MainScreen.Bounds);
+
+			App.SetAdapter (Adapter.Current);
+
+			LoadApplication (new App ());
+			
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+

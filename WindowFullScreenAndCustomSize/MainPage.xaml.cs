2bb4975ffa6af616862558d5dab0322d03c8ad22
El Bruno <bcapuano@gmail.com>
2015-12-14 18:06:05 +0100

    + sample to change from full screen to normal
    screen and also to have a custom app size

---
+++ WindowFullScreenAndCustomSize/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Windows.Foundation;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+
+namespace App1
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var currentView = ApplicationView.GetForCurrentView();
+            if (!currentView.IsFullScreen)
+                currentView.TryEnterFullScreenMode();
+            else
+            {
+                currentView.ExitFullScreenMode();
+                currentView.TryResizeView(new Size(767, 383));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var currentView = ApplicationView.GetForCurrentView();
+            currentView.TryResizeView(new Size(767, 383));
+        }
+    }
+}

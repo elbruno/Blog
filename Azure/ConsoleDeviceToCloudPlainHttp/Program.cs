0006dd033034bb6df3e6f41106dc544e938ffcca
El Bruno <bcapuano@gmail.com>
2016-03-11 08:49:41 -0500

    Complete sample to send data using HTTPClient to an azure iot hub

--- Azure/ConsoleDeviceToCloudPlainHttp/Program.cs
+++ Azure/ConsoleDeviceToCloudPlainHttp/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace ConsoleDeviceToCloudPlainHttp
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using PclAzureIoT;
+
+namespace ConsoleDeviceToCloudPlainHttp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var url = $"{Config.EventHubCompatibleName}/publishers/{Config.DeviceId}/messages";
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine("send for " + i);
+                Console.ReadLine();
+                var httpClient = new HttpClient { BaseAddress = new Uri(Config.EventHubUri) };
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", Config.SharedAccessSignature);
+                var message = @"{""data"" : {""Bpm"": 123 ,""Steps"": " + i + @" }}";
+                var content = new StringContent(message, Encoding.UTF8, "application/json");
+                content.Headers.Add("ContentType", "application/atom+xml");
+                httpClient.PostAsync(url, content);
+            }
         }
     }
 }
--- Azure/PclAzureIoT/Config.cs
+++ Azure/PclAzureIoT/Config.cs
@@ -4,11 +4,15 @@
     {
         public const string ConnectionString = "{iothub connection string}";
         public const string ConnectionStringAzureSbLite = "{Azure SB Lite connection string}";
+        public const string EventHubName = "{EventHub Name }";
+        public const string EventHubUri = "{EventHub Uri}";
+        public const string EventHubUriFieldGateway = "{EventHub Uri Field Gateway }";
         public const string EventHubCompatibleName = "{Event Hub Compatible Name }";
         public const string EventHubCompatibleEndPoint = "{Event Hub Compatible EndPoint }";
-        public const string DeviceId = "{devide id}";
+        public const string DeviceId = "Device01";
         public const string IotHubD2CEndpoint = "messages/events";
         public const string IotHubUri = "{iot hub hostname}";
         public const string DeviceKey = "{device key}";
+        public const string SharedAccessSignature = "{SharedAccessSignature}";
     }
 }

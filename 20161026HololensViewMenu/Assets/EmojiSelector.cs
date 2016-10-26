b0058c13f0c97fbd93da4fb03f713465912defd9
elbruno <bcapuano@gmail.com>
2016-10-25 23:06:59 -0400

    add view menu sample

---
+++ 20161026HololensViewMenu/Assets/EmojiSelector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class EmojiSelector : MonoBehaviour
+{
+    void Start() {}
+    void OnGazeEnter() {}
+    void OnGazeLeave() {}
+    void Update() {}
+    void OnSelect()
+    {
+        var hit = HoloToolkit.Unity.GazeManager.Instance.HitInfo;
+        if (hit.transform.gameObject == null) return;
+        var gameObjectName = hit.transform.gameObject.name;
+        var textResult = GameObject.Find("ResultText");
+        textResult.GetComponent<TextMesh>().text = gameObjectName;
+    }
+}
---
+++ 20161026HololensViewMenu/Assets/SpeechManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechManager : MonoBehaviour
+{
+    private KeywordRecognizer _keywordRecognizer = null;
+    private readonly Dictionary<string, System.Action> _keywords = new Dictionary<string, System.Action>();
+    private bool _visible = false;
+
+    private void Start()
+    {
+        _keywords.Add("Menu", ToggleVisibility);
+
+        _keywordRecognizer = new KeywordRecognizer(_keywords.Keys.ToArray());
+        _keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
+        _keywordRecognizer.Start();
+    }
+
+    private void ToggleVisibility()
+    {
+        var holoMenu = GameObject.Find("HoloMenu");
+        if (holoMenu == null) return;
+
+        _visible = !_visible;
+        var rend = holoMenu.GetComponent<Renderer>();
+        if (rend != null) rend.enabled = _visible;
+
+        holoMenu.transform.localScale = Vector3.one * 0.3f;
+        holoMenu.transform.position = 
+            Camera.main.transform.position + Camera.main.transform.forward;
+    }
+
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        System.Action keywordAction;
+        if (_keywords.TryGetValue(args.text, out keywordAction))
+        {
+            keywordAction.Invoke();
+        }
+    }
+}

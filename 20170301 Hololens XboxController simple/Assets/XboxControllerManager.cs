01efd2b757d47475fde7babb3b3e3bf9c6f1f16f
El Bruno <bcapuano@gmail.com>
2017-02-28 23:06:18 -0500

    add sample for Hololens and XBoxOne controller

---
+++ 20170301 Hololens XboxController simple/Assets/XboxControllerManager.cs
@@ -0,0 +1,14 @@
+﻿using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+public class XboxControllerManager : MonoBehaviour
+{
+
+    void Start() { }
+
+    void Update()
+    {
+        var buttonA = Input.GetButton("Fire1");
+        SpatialMappingManager.Instance.DrawVisualMeshes = buttonA;
+    }
+}

bc963af9a3a834700dc8272b496bc297f8d4905d
El Bruno <bcapuano@gmail.com>
2017-03-13 23:24:02 -0400

    Add Holotolkit Airtap click sample to blog

---
+++ 20170314 HoloToolkit AirTap Click/Assets/Scripts/CubeManager.cs
@@ -0,0 +1,19 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public class CubeManager : MonoBehaviour, IInputClickHandler, IInputHandler
+{
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        // AirTap code goes here
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+    }
+
+    public void OnInputUp(InputEventData eventData)
+    {
+    }
+}

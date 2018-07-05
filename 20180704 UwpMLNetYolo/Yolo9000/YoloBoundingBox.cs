56072d6ca5871ce627c3dba5e31cbb49b183d224
El Bruno <elbruno@users.noreply.github.com>
2018-07-04 23:48:24 -0400

    Windows10 App using Tiny-YoloV2 for object detection
    Windows10 App using Tiny-YoloV2 for object detection

---
+++ 20180704 UwpMLNetYolo/Yolo9000/YoloBoundingBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+
+namespace UwpAppYolo01.Yolo9000
+{
+    class YoloBoundingBox
+    {
+        public string Label { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public float Height { get; set; }
+        public float Width { get; set; }
+
+        public float Confidence { get; set; }
+
+        public RectangleF Rect
+        {
+            get { return new RectangleF(X, Y, Width, Height); }
+        }
+
+        public override string ToString()
+        {
+            return $"Label: {Label}, X: {X}, Y: {Y}, Height: {Height}, Width: {Width}, Confidence: {Confidence}";
+        }
+    }
+}

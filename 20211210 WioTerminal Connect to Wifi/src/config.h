33be345ac5260619a8bc8ceccc928e32390a3caa
El Bruno <elbruno@users.noreply.github.com>
2021-12-10 10:30:51 -0500

    remove info

--- 20211210 WioTerminal Connect to Wifi/src/config.h
+++ 20211210 WioTerminal Connect to Wifi/src/config.h
@@ -5,8 +5,5 @@
 using namespace std;
 
 // WiFi credentials
-const char *SSID = "IoTLabs";
-const char *PASSWORD = "12345678";
-
-// IoT Hub settings
-const char *CONNECTION_STRING = "HostName=elbrunoiot.azure-devices.net;DeviceId=Wio02;SharedAccessKey=y7AJ4a6YjD98bcKs1Iog4jMD5lakD1qhblsztt6UfPc=";
+const char *SSID = "SSID";
+const char *PASSWORD = "Password";
